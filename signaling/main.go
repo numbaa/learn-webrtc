40379402bc9e007872b9491b40ce6ae644d22788
Zhennan Tu <zhennan.tu@gmail.com>
2020-04-26 01:33:49 +0800

    添加信令服务器

--- signaling/main.go
+++ signaling/main.go
@@ -2,24 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{}
-
-func signaling(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	//TODO: xxxx
-	c.ReadMessage()
-}
 
 func main() {
 	http.HandleFunc("/signaling", signaling)
---
+++ signaling/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+	sessions = make(map[string]*websocket.Conn)
+	mtx      = sync.Mutex{}
+)
+
+func signaling(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrade:", err)
+		return
+	}
+	//TODO: xxxx
+	mtx.Lock()
+	sessions[c.RemoteAddr().String()] = c
+	mtx.Unlock()
+	for {
+		t, msg, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println("ReadMessage error:", err)
+			return
+		}
+		if t != websocket.TextMessage {
+			fmt.Println("Unsupported message type:", t)
+			continue
+		}
+		mtx.Lock()
+		for addr, conn := range sessions {
+			if addr == c.RemoteAddr().String() {
+				continue
+			}
+			fmt.Println("Relay message to ", addr)
+			conn.WriteMessage(websocket.TextMessage, msg)
+		}
+		mtx.Unlock()
+	}
+}
